f65c228591cfac96a839fd007371ef910352faca
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-17 10:35:21 -0400

    Pass test

--- map.go
+++ map.go
@@ -1,5 +1,17 @@
 package suggestionmap
 
+import (
+	"fmt"
+	"path"
+	"regexp"
+)
+
+const (
+	test = `(.*):(\d+):(\d+) Cannot find '(.+?)' in scope`
+)
+
 func FindSuggestions(context string, input string) string {
-	return ""
+	re := regexp.MustCompile(test)
+	matches := re.FindStringSubmatch(input)
+	return fmt.Sprintf("Make sure that %v is defined above line %v in %v", matches[4], matches[2], path.Base(matches[1]))
 }
